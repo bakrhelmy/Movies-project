f869ed9648af8d75d49fb45228338384d72734ae
bakrhelmy <abubakrhelmy@gmail.com>
2026-01-18 02:51:25 +0200

    update2

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import StarRatting from "./StarRatting";
-// import { useState } from "react";
-// function Test() {
-//   const [movieRatting, setMovieRatting] = useState(0);
-//   return (
-//     <div>
-//       <StarRatting color="blue" maxRating={10} onSetRating={setMovieRatting} />
-//       <p>this movie rating is {movieRatting}</p>
-//     </div>
-//   );
-// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
